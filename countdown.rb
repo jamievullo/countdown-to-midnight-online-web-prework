ca5bb4366cace738ce12e1bd6455beeb05a7bc13
Jamie Vullo <jvullo@protonmail.com>
2019-05-03 17:35:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,18 +2,18 @@
 
 def countdown(num)
   while num > 0 do 
-  sleep 1
-  puts "#{num} SECOND(S)!"
-  num -= 1
+    sleep 1
+    puts "#{num} SECOND(S)!"
+   num -= 1
   end
-  sleep 1 
+    sleep 1 
   return "HAPPY NEW YEAR!"
 end 
 
 def countdown_with_sleep(num)
   while num > 0 do 
     sleep 1 
-    puts "#{num} SECOND(S)"
+      puts "#{num} SECOND(S)"
     num -= 1 
     
   end
